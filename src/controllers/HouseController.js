f383662be3211f3eff70ed2702fbe74da0fe9d05
melanie <mjharv@hotmail.com>
2024-11-19 16:06:48 -0700

    Working api

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,4 +1,5 @@
-import { houseService } from "../services/HouseService"
+import { request, response } from "express"
+// import { houseService } from "../services/HouseService"
 import BaseController from "../utils/BaseController"
 
 export class HouseController extends BaseController {
@@ -6,24 +7,29 @@ export class HouseController extends BaseController {
         super('api/houses')
         this.router
             .get('', this.getHouse)
-            .get('/:houseId', this.getHouseById)
+        // .get('/:houseId', this.getHouseById)
     }
-    async getHouseById(request, response, next) {
-        try {
-            const houseId = request.params.houseId
-            const house = await houseService.getHouseById(houseId)
-            response.send(house)
-        } catch (error) {
-            next(error)
-        }
-    }
+    // async getHouseById(request, response, next) {
+    //     try {
+    //         const houseId = request.params.houseId
+    //         const house = await houseService.getHouseById(houseId)
+    //         response.send(house)
+    //     } catch (error) {
+    //         next(error)
+    //     }
+    // }
+    // @param {import("express").Resquest} request
+    // @param {import("express").Response} response
+    // @param {import("express").NextFunction} next
+
     async getHouse(request, response, next) {
-        try {
-            const houseQuery = request.query
-            const houses = await houseService.getHouse(houseQuery)
-            response.send(houses)
-        } catch (error) {
-            next(error)
-        }
+        response.send('House api is working')
+        // try {
+        //     const houseQuery = request.query
+        //     const houses = await houseService.getHouse(houseQuery)
+        //     response.send(houses)
+        // } catch (error) {
+        //     next(error)
+        // }
     }
 }
